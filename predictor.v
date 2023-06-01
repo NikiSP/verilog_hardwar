ffbbc816e0f5111980d61b8db242061e9f17236a
Asemaneh Nafe <asemanehnafe@gmail.com>
2023-06-01 10:34:09 +0330

    1

--- predictor.v
+++ predictor.v
@@ -2,4 +2,23 @@ module predictor(input wire request, result, clk, taken, output reg prediction);
 
 // Your code
 
+reg [1:0]PHS;
+
+always @(request==1'b1 , posedge clk) begin
+  prediction=PHS[1];
+end
+
+always @(result==1'b1 , posedge clk) begin
+  if(taken) begin
+    if(PHS<2'b11)begin
+      PHS<=PHS+1;
+    end
+  end
+  else begin
+    if(PHS>2'b00) begin
+      PHS<=PHS-1;  
+    end
+  end
+end
+
 endmodule
